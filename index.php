8337eb70050a9c800dbe91d73923359e89e115b8
yaoshanliang <1329517386@qq.com>
2015-01-30 17:34:30 +0800

    update snow in app

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	<script type="text/javascript" src="http://api.map.baidu.com/api?v=1.5&ak=C1e4e2b050755a542005cdb306e22788"></script>
 	<script src="http://map.qq.com/api/js?v=2.exp&key=d84d6d83e0e51e481e50454ccbe8986b"></script>
     <script type="text/javascript" src="./js/jsized.snow.min.js"></script>
- 
 </head>
 
 
@@ -116,7 +115,7 @@
 		</ul>
 		<script>
 			$(document).ready(function() {initMenu();});
-			createSnow('./images', 60);
+			createSnow('./images/', 60);
     	</script>
 	</div>
 </div>
